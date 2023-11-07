2f2e3b1e68929bc4746bd6d61fb5c3c906507d75
WaRtrO <morotmaxence@live.fr>
2023-11-07 17:28:34 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd $HOME
+git clone https://github.com/WaRtrO89/francinette-image.git
+cd $HOME/francinette-image
+chmod +x run.sh
+docker build -t . francinette-image
+
+RC_FILE="$HOME/.zshrc"
+
+printf "\nif ! docker ps | grep "francinette-image" &> /dev/null; then" "$HOME" >> "$RC_FILE"
+printf "\ndocker run -d -i -v /home:/home -t --name image_francinette francinette-image /bin/bash" "$HOME" >> "$RC_FILE"
+printf "\nfi" "$HOME" >> "$RC_FILE"
+
+if ! grep "francinette=" "$RC_FILE" &> /dev/null; then
+	echo "francinette alias not present"
+	printf "\nalias francinette=%s/francinette-image/run.sh\n" "$HOME" >> "$RC_FILE"
+fi
+
+if ! grep "paco=" "$RC_FILE" &> /dev/null; then
+	echo "Short alias not present. Adding it"
+	printf "\nalias paco=%s/francinette-image/run.sh\n" "$HOME" >> "$RC_FILE"
+fi
