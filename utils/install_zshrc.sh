dfc867c92d84a00355a1987f7d7d9369c10885c0
Maxence <morotmaxence@live.fr>
2024-11-02 18:34:40 +0100

    Update install_zshrc.sh

--- utils/install_zshrc.sh
+++ utils/install_zshrc.sh
@@ -2,6 +2,26 @@
 
 RC_FILE="$HOME/.zshrc"
 
+#if ! grep "francinette-image" "$RC_FILE" &> /dev/null; then
+#printf "\nif ! systemctl status docker | grep "running" &> /dev/null; then" "$HOME" >> "$RC_FILE"
+#printf "\n\t\techo \"[Francinette] Starting Docker...\"" "$HOME" >> "$RC_FILE"
+#printf "\n\t\tsleep 1" "$HOME" >> "$RC_FILE"
+#printf "\n\t\texec \"$SHELL\"" "$HOME" >> "$RC_FILE"
+#printf "\nfi" "$HOME" >> "$RC_FILE"
+#
+#printf "\nif ! docker image ls | grep "francinette-image" &> /dev/null; then" "$HOME" >> "$RC_FILE"
+#printf "\n\t\techo \"[Francinette] Loading the docker container\"" "$HOME" >> "$RC_FILE"
+#printf "\n\t\tdocker load < %s/francinette-image/francinette.tar" "$HOME" >> "$RC_FILE"
+#printf "\n\t\texec \"$SHELL\"" "$HOME" >> "$RC_FILE"
+#printf "\nfi" "$HOME" >> "$RC_FILE"
+#
+#printf "\nif ! docker ps | grep \"francinette-image\" &> /dev/null; then" "$HOME" >> "$RC_FILE"
+#printf "\n\tif docker run -d -i -v /home:/home -v /goinfre:/goinfre -v /sgoinfre:/sgoinfre -v %s/francinette-image/logs:/francinette/logs-t --name run-paco francinette-image /bin/bash 2>&1 | grep \"already\" &> /dev/null; then" "$HOME" >> "$RC_FILE"
+#printf "\n\t\tdocker start run-paco" "$HOME" >> "$RC_FILE"
+#printf "\n\tfi" "$HOME" >> "$RC_FILE"
+#printf "\nfi" "$HOME" >> "$RC_FILE"
+#fi
+
 if ! grep "francinette-image" "$RC_FILE" &> /dev/null; then
 printf "\nif ! systemctl status docker | grep "running" &> /dev/null; then" "$HOME" >> "$RC_FILE"
 printf "\n\t\techo \"[Francinette] Starting Docker...\"" "$HOME" >> "$RC_FILE"
@@ -10,9 +30,19 @@ printf "\n\t\texec \"$SHELL\"" "$HOME" >> "$RC_FILE"
 printf "\nfi" "$HOME" >> "$RC_FILE"
 
 printf "\nif ! docker image ls | grep "francinette-image" &> /dev/null; then" "$HOME" >> "$RC_FILE"
-printf "\n\t\techo \"[Francinette] Loading the docker container\"" "$HOME" >> "$RC_FILE"
-printf "\n\t\tdocker load < %s/francinette-image/francinette.tar" "$HOME" >> "$RC_FILE"
+printf "\n\tif docker image ls 2>&1 | grep permission &> /dev/null; then" "$HOME" >> "$RC_FILE"
+printf "\n\t\techo \"[Francinette] Permission Error\"" "$HOME" >> "$RC_FILE"
+printf "\n\telse" "$HOME" >> "$RC_FILE"
+printf "\n\t\tif ! [ -f %s/francinette-image/francinette.tar ] || docker load < %s/francinette-image/francinette.tar 2>&1 | grep \"no such file\"  &> /dev/null; then" "$HOME" "$HOME" >> "$RC_FILE"
+printf "\n\t\t\techo \"[Francinette] Error width file \"francinette.tar\"\"" "$HOME" >> "$RC_FILE"
+printf "\n\t\t\tdocker build -t francinette-image %s/francinette-image" "$HOME" >> "$RC_FILE"
+printf "\n\t\t\tdocker image save francinette-image > %s/francinette-image/francinette.tar" "$HOME" >> "$RC_FILE"
+printf "\n\t\telse" "$HOME" >> "$RC_FILE"
+printf "\n\t\t\techo \"[Francinette] Loading the docker container\"" "$HOME" >> "$RC_FILE"
+printf "\n\t\t\tdocker load < %s/francinette-image/francinette.tar" "$HOME" >> "$RC_FILE"
+printf "\n\t\tfi" "$HOME" >> "$RC_FILE"
 printf "\n\t\texec \"$SHELL\"" "$HOME" >> "$RC_FILE"
+printf "\n\tfi" "$HOME" >> "$RC_FILE"
 printf "\nfi" "$HOME" >> "$RC_FILE"
 
 printf "\nif ! docker ps | grep \"francinette-image\" &> /dev/null; then" "$HOME" >> "$RC_FILE"
@@ -21,6 +51,7 @@ printf "\n\t\tdocker start run-paco" "$HOME" >> "$RC_FILE"
 printf "\n\tfi" "$HOME" >> "$RC_FILE"
 printf "\nfi" "$HOME" >> "$RC_FILE"
 fi
+
 
 if ! grep "francinette=" "$RC_FILE" &> /dev/null; then
 	printf "\nalias francinette=%s/francinette-image/run.sh\n" "$HOME" >> "$RC_FILE"
